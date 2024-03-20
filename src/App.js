7e3e5cabc1e3ba429f7d41f6d9741bc0465af4dd
monicaVaideanu <123547873+monicaVaideanu@users.noreply.github.com>
2024-03-20 12:36:54 +0200

    start watch

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import './App.css';
 import Counter from './counter';
 import WelcomeMessage from './welcomeMessage'
+import Stopwatch from "./Stopwatch";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
       {/* <Counter>Counter App</Counter> */}
-     <WelcomeMessage></WelcomeMessage>
+     {/* <WelcomeMessage></WelcomeMessage> */}
+    <Stopwatch></Stopwatch>
      </div>
     );
   }
