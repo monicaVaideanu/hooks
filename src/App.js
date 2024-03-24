4504511f4e1147af736a39cee53718a1b75414ed
monicaVaideanu <123547873+monicaVaideanu@users.noreply.github.com>
2024-03-24 10:49:57 +0200

    stopwatch homework

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css';
 import Counter from './counter';
 import WelcomeMessage from './welcomeMessage'
-import Stopwatch from "./Stopwatch";
+import Stopwatch from "./Stopwatch.jsx";
 
 class App extends React.Component {
   render() {
--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,1 +1,66 @@
 import React, { useState, useEffect } from 'react';
+import "./Stopwatch.css";
+
+const Stopwatch = () => {
+    const [startTime, setStartTime] = useState(null);
+    const [pauseTime, setPauseTime] = useState(null);
+    const [time, setTime] = useState("00:00:00");
+
+    useEffect(() => {
+        let interval;
+        if (startTime && !pauseTime) {
+            interval = setInterval(updateStopwatch, 1000);
+        }
+        return () => clearInterval(interval);
+    }, [startTime, pauseTime]);
+
+    const startStopwatch = () => {
+        if (!startTime || pauseTime) {
+            const newStartTime = pauseTime ? new Date(new Date() - (pauseTime - startTime)) : new Date();
+            setStartTime(newStartTime);
+            setPauseTime(null);
+        }
+    };
+    
+
+    const stopWatch = () => {
+        if (startTime && !pauseTime) {
+            setPauseTime(new Date());
+        }
+    };
+
+    const resetStopwatch = () => {
+        setStartTime(null);
+        setPauseTime(null);
+        setTime("00:00:00");
+    };
+
+    const updateStopwatch = () => {
+        const currentTime = new Date();
+        const elapsed = pauseTime ? pauseTime.getTime() - startTime.getTime() : currentTime.getTime() - startTime.getTime();
+        const seconds = Math.floor(elapsed / 1000) % 60;
+        const minutes = Math.floor(elapsed / (1000 * 60)) % 60;
+        const hours = Math.floor(elapsed / (1000 * 60 * 60));
+
+        const displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+        setTime(displayTime);
+    };
+
+    const pad = (number) => {
+        return (number < 10 ? "0" : "") + number;
+    };
+
+    return (
+        <div>
+            <h1> Stopwatch</h1>
+            <h2>Time passed: {time}</h2>
+                <div className='button-container'>
+                <button id="Start" onClick={startStopwatch}>Start</button>
+                <button id="Stop" onClick={stopWatch}>Stop</button>
+                <button id="Reset" onClick={resetStopwatch}>Reset</button>
+                </div>
+        </div>
+    );
+};
+
+export default Stopwatch;
